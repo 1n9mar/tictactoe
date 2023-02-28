eb8d314f357bd170ad9c4c91aab8f08326269b04
Ingmar Nebel <ingmar.nebel@ergon.ch>
2023-03-01 00:10:18 +0100

    move square state management up to the board component

--- src/App.js
+++ src/App.js
@@ -2,29 +2,35 @@ import React from 'react';
 import { useState } from 'react';
 
 export default function Board() {
-  return (<React.Fragment> {  Array(3).fill(null).map((_, rowNum) => row(rowNum)) }
-  </React.Fragment>);
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
   function row(rowNum) {
     return <div className="board-row">
-      { Array(3).fill(0).map(colNum => 
-      <Square />
+      { Array(3).fill(0).map((_, colNum) => 
+      <Square 
+      value={squares[rowNum*3+colNum]}
+      onSquareClick={() => handleClick(rowNum*3+colNum)}
+      />
       )}
     </div>;
   }
 
-  function Square() {
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-      setValue('X');
-    }
-
+  function Square({value, onSquareClick}) {
     return <button 
     className="square"
-    onClick={handleClick}>
+    onClick={onSquareClick}>
       {value}
     </button>;
   }
+
+  function handleClick(squareNo) {
+    const nextSquares = squares.slice();
+    nextSquares[squareNo] = "X";
+    setSquares(nextSquares);
+  }
+
+  return (<React.Fragment> {  Array(3).fill(null).map((_, rowNum) => row(rowNum)) }
+  </React.Fragment>);
+
 }
 
