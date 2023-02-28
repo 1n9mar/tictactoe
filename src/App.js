f873e594e553f15502572361556b9abf2c5b32ef
Ingmar Nebel <ingmar.nebel@ergon.ch>
2023-02-28 17:03:30 +0100

    fill board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return (<React.Fragment> {  [1, 2, 3].map(rowNum => row(rowNum)) }
+  </React.Fragment>);
+
+  function row(rowNum) {
+    return <div className="board-row">{ [1, 2, 3].map(colNum => cell(rowNum, colNum)) }</div>;
+  }
+
+  function cell(rowNum, colNum) {
+    return <button className="square">X</button>;
+  }
 }
+
